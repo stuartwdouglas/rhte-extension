04e33d04f82cf160a20395b8b0dece5ba4c1e3ad
Stuart Douglas <stuart.w.douglas@gmail.com>
2019-09-10 09:46:59 +1000

    First hello world

--- deployment/src/main/java/com/github/stuartwdouglas/rhteextension/HelloWorldBuildStep.java
+++ deployment/src/main/java/com/github/stuartwdouglas/rhteextension/HelloWorldBuildStep.java
@@ -1,5 +1,15 @@
 package com.github.stuartwdouglas.rhteextension;
 
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.annotations.ExecutionTime;
+import io.quarkus.deployment.annotations.Record;
+
 public class HelloWorldBuildStep {
 
+    @BuildStep
+    @Record(ExecutionTime.RUNTIME_INIT)
+    void sayHello(HelloWorldRecorder recorder) {
+        System.out.println("About to say Hello");
+        recorder.sayHello("Stuart");
+    }
 }
--- runtime/src/main/java/com/github/stuartwdouglas/rhteextension/HelloWorldRecorder.java
+++ runtime/src/main/java/com/github/stuartwdouglas/rhteextension/HelloWorldRecorder.java
@@ -5,5 +5,8 @@ import io.quarkus.runtime.annotations.Recorder;
 @Recorder
 public class HelloWorldRecorder {
 
+    public void sayHello(String name) {
+        System.out.println("Hello " + name);
+    }
 
 }
